55c83e81629f71f340fa3151122f283f147bceb4
George Linkovsky <Timofffee@users.noreply.github.com>
2017-03-27 05:36:34 +0400

    Add files via upload

--- Node.gd
+++ Node.gd
@@ -4,10 +4,15 @@ extends Node
 var ip = "weber.freenode.net"
 var client
 var nick = "GodotIRC"
-var channel = "#godotengine"
+var channel = "#godotengine_ru"
 var password = ""
 
+var time
+
+var lt = RichTextLabel.new()
+
 func _ready():
+	_on_time_updater_timeout()
 	client = StreamPeerTCP.new()
 	client.connect(ip, 6667)
 	if password != "":
@@ -19,7 +24,7 @@ func _ready():
 
 func _fixed_process(delta):
 	if client.is_connected() && client.get_available_bytes() >0:
-		var time = str(OS.get_time().hour) + ":" + str(OS.get_time().minute) + ":" + str(OS.get_time().second)
+		
 		var a = str(client.get_utf8_string(client.get_available_bytes()))
 		a = a.split('\n')
 		for b in a:
@@ -31,32 +36,52 @@ func _fixed_process(delta):
 					var text = ""
 					for i in range (4, b.size()):
 						text += b[i] + " "
-					get_node("text").append_bbcode("[" + time + "] SERVER: " + text + "\n")
+					get_node("text").append_bbcode("[" + time + "][i] [color=green]SERVER:[/color] " + text + "[/i]\n")
 				elif b[1] == "JOIN":
-					get_node("text").append_bbcode("[" + time + "] == " + get_name_user(b[0]) + " has joined " + str(channel) + "\n")
+					get_node("text").append_bbcode("[" + time + "][i] [color=red]==[/color] " + get_name_user(b[0]) + " has joined " + str(channel) + "[/i]\n")
 				elif b[1] == "QUIT":
 					var text = ""
 					for i in range (4, b.size()):
 						text += b[i] + " "
-					get_node("text").append_bbcode("[" + time + "] == " + get_name_user(b[0]) + " QUIT (" + str(text) + ")\n")
+					get_node("text").append_bbcode("[" + time + "][i] == " + get_name_user(b[0]) + " QUIT (" + str(text) + ")[/i]\n")
 				elif b[1] == "PART":
-					get_node("text").append_bbcode("[" + time + "] == " + get_name_user(b[0]) + " has left " + str(channel) + "\n")
+					get_node("text").append_bbcode("[" + time + "][i] == " + get_name_user(b[0]) + " has left " + str(channel) + "[/i]\n")
 				elif b[1] == "PRIVMSG":
 					var text = ""
 					for i in range (3, b.size()):
 						text += b[i] + " "
 					text = text.substr(1, text.length()-1)
+					lt.clear()
+					lt.append_bbcode(text)
 					get_node("text").append_bbcode("[" + time + "] <" + get_name_user(b[0]) + "> " + str(text) + "\n")
+
+
 
 func get_name_user (value):
 	var a = value.find("!")
 	return value.substr(1, a-1)
 
 func _on_enterLine_text_entered( text ):
-	var time = str(OS.get_time().hour) + ":" + str(OS.get_time().minute) + ":" + str(OS.get_time().second)
-	get_node("text").append_bbcode("[" + time + "] <" + nick + "> " + str(text) + "\n")
-	client.put_data(("PRIVMSG "+ channel + " :" + str(text) +"\n").to_utf8())
+	lt.clear()
+	lt.append_bbcode(text)
+	get_node("text").append_bbcode("[" + time + "] <" + nick + "> " + str(lt.get_text()) + "\n")
+	client.put_data(("PRIVMSG "+ channel + " :" + str(lt.get_text()) +"\n").to_utf8())
 	get_node("enterLine").clear()
 
 func _on_Button_pressed():
 	_on_enterLine_text_entered(get_node("enterLine").get_text())
+
+
+func _on_time_updater_timeout():
+	time = ""
+	if OS.get_time().hour < 10:
+		time += "0"
+	time += str(OS.get_time().hour) + ":"
+	
+	if OS.get_time().minute < 10:
+		time += "0"
+	time += str(OS.get_time().minute) + ":"
+	
+	if OS.get_time().second < 10:
+		time += "0"
+	time += str(OS.get_time().second)
